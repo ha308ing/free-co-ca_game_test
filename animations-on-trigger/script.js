abfda1e1ccb8f179b5b92cbe152a7e70bcb29695
Ivan Arkaev <75647984+ha308ing@users.noreply.github.com>
2024-01-19 04:27:22 +0400

    animate boom on click and move

--- animations-on-trigger/script.js
+++ animations-on-trigger/script.js
@@ -11,42 +11,84 @@ const explosions = [];
 
 class Explosion {
   constructor(x, y) {
-    this.x = x;
-    this.y = y;
     this.spriteWidth = 200;
     this.spriteHeight = 179;
     this.width = this.spriteWidth * 0.5;
     this.height = this.spriteHeight * 0.5;
+    this.x = x;
+    this.y = y;
     this.image = new Image();
     this.image.src = "./boom.png";
     this.frame = 0;
+    this.timer = 0;
+    // random angle from 0 to 360 deg (2PI)
+    this.angle = Math.random() * 6.28;
   }
 
   update() {
-    this.frame++;
+    this.timer++;
+    if (this.timer % 7 === 0) {
+      this.frame++;
+    }
   }
 
   draw() {
+    // save current state of the canvas
+    // the following changes affect only next draw call
+    ctx.save();
+
+    // change coornates origin (translate rotation center)
+    ctx.translate(this.x, this.y);
+
+    // rotate entire canvas context
+    ctx.rotate(this.angle);
+
     ctx.drawImage(
       this.image,
-      (this.frame % 6) * this.spriteWidth,
+      this.frame * this.spriteWidth,
       0,
       this.spriteWidth,
       this.spriteHeight,
-      this.x - this.width / 2,
-      this.y - this.height / 2,
+      0 - this.width * 0.5, // this.x is captured in translate,
+      // this.x,
+      0 - this.height * 0.5, // this.y is captured in translate,
+      // this.y,
       this.width,
       this.height
     );
+
+    // restore canvas context to the original save point
+    ctx.restore();
   }
 }
 
 window.addEventListener("click", event => {
+  createAnimation(event);
+});
+
+// window.addEventListener("mousemove", event => {
+//   createAnimation(event);
+// });
+
+function createAnimation(event) {
   // const x = event.layerX;
   // const y = event.layerY;
   const positionX = event.x - canvasPosition.left;
   const positionY = event.y - canvasPosition.top;
-  const e = new Explosion(positionX, positionY);
-  e.draw();
-});
-const e = new Explosion(10, 10);
+  explosions.push(new Explosion(positionX, positionY));
+}
+
+function animate() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  for (let i = 0; i < explosions.length; i++) {
+    explosions[i].update();
+    explosions[i].draw();
+    if (explosions[i].frame > 5) {
+      explosions.splice(i, 1);
+      i--;
+    }
+  }
+  requestAnimationFrame(animate);
+}
+
+animate();
