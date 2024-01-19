1339c6f08fb649903df07779e2fd4e9f68db99a2
Ivan Arkaev <75647984+ha308ing@users.noreply.github.com>
2024-01-19 17:26:28 +0400

    add interval raven spawn

--- draft/script.js
+++ draft/script.js
@@ -5,6 +5,16 @@ let CANVAS_HEIGHT = (canvas.height = window.innerHeight);
 
 const ctx = canvas.getContext("2d");
 
+// accumulate time values between frames
+let timeToNextRaven = 0;
+
+// time to when timeToNextRaven accumulated to ravenInterval value
+// trigger next raven and reset, timeToNextRaven to start again
+let ravenInterval = 5000;
+
+// timestamp value from previous value
+let lastTime = 0;
+
 let ravens = [];
 let gameFrame = 0;
 
@@ -23,12 +33,16 @@ class Raven {
     this.frame = 0;
     this.numberOfSpites = 5;
     this.staggerFrames = 5;
+    this.markedForDelete = false;
   }
 
   update() {
     this.x -= this.directionX;
     if (gameFrame % this.staggerFrames == 0) {
       this.frame < this.numberOfSpites ? this.frame++ : (this.frame = 0);
+    }
+    if (this.x < -this.width) {
+      this.markedForDelete = true;
     }
   }
 
@@ -52,12 +66,33 @@ class Raven {
 
 const raven = new Raven();
 
+// timestamp to refer to ms not computer speed
+// draw next frame after timestamp, not just when finish calcs
 function animate(timestamp) {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   gameFrame++;
-  raven.update();
-  raven.draw();
+  // raven.update();
+  // raven.draw();
+
+  // time in ms between frames
+  let deltaTime = timestamp - lastTime;
+  lastTime = timestamp;
+  timeToNextRaven += +deltaTime;
+  if (timeToNextRaven > ravenInterval) {
+    // add new raven every timeToNextRaven
+    ravens.push(new Raven());
+    timeToNextRaven = 0;
+  }
+
+  [...ravens].forEach(raven => {
+    raven.update();
+    raven.draw();
+  });
+
+  ravens = ravens.filter(r => !r.markedForDelete);
+  console.log(ravens);
+
   requestAnimationFrame(animate);
 }
 
-animate();
+animate(0);
