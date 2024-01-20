491ff6528a0720105919f72c0089a54ab01d9d64
Ivan Arkaev <75647984+ha308ing@users.noreply.github.com>
2024-01-20 15:53:47 +0400

    start side scroller game

---
+++ side-scroller/script.js
@@ -0,0 +1,21 @@
+window.addEventListener("load", () => {
+  /** @type {HTMLCanvasElement} */
+  const canvas = document.getElementById("canvas1");
+  const ctx = canvas.getContext("2d");
+  canvas.width = 800;
+  canvas.height = 720;
+
+  class InputHander {}
+
+  class Player {}
+
+  class Background {}
+
+  class Enemy {}
+
+  function handleEnemies() {}
+
+  function displayStatusText() {}
+
+  function animate() {}
+});
