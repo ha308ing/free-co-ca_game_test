9b05f778127b38a7ecb59bd3a2f9717207d4b6bc
Ivan Arkaev <75647984+ha308ing@users.noreply.github.com>
2024-01-21 02:32:26 +0400

    sitting state

--- state-management/state.js
+++ state-management/state.js
@@ -1,6 +1,8 @@
 export const state = {
   STANDING_LEFT: 0,
   STANDING_RIGHT: 1,
+  SITTING_LEFT: 2,
+  SITTING_RIGHT: 3,
 };
 
 class State {
@@ -26,6 +28,8 @@ export class StandingLeft extends State {
     if (input === "PRESS right") {
       // set state to StandingRight
       this.player.setState(state.STANDING_RIGHT);
+    } else if (input === "PRESS down") {
+      this.player.setState(state.SITTING_LEFT);
     }
   }
 }
@@ -42,10 +46,55 @@ export class StandingRight extends State {
   }
 
   handleInput(input) {
-    console.log(input);
     if (input === "PRESS left") {
       // set state to StandingLeft
       this.player.setState(state.STANDING_LEFT);
+    } else if (input === "PRESS down") {
+      this.player.setState(state.SITTING_RIGHT);
     }
+  }
+}
+
+export class SittingLeft extends State {
+  constructor(player) {
+    super("SITTING LEFT");
+
+    this.player = player;
+  }
+
+  enter() {
+    this.player.frameY = 9;
+  }
+
+  handleInput(input) {
+    if (input === "PRESS right") this.player.setState(state.SITTING_RIGHT);
+    else if (input === "RELEASE down") {
+      this.player.setState(state.STANDING_LEFT);
+    }
+    /*
+      else if (input === "PRESS up") this.player.setState(state.STANDING_LEFT);
+    */
+  }
+}
+
+export class SittingRight extends State {
+  constructor(player) {
+    super("SITTING RIGHT");
+
+    this.player = player;
+  }
+
+  enter() {
+    this.player.frameY = 8;
+  }
+
+  handleInput(input) {
+    if (input === "PRESS left") this.player.setState(state.SITTING_LEFT);
+    else if (input === "RELEASE down") {
+      this.player.setState(state.STANDING_RIGHT);
+    }
+    /*
+      else if (input === "PRESS up") this.player.setState(state.STANDING_RIGHT);
+     */
   }
 }
