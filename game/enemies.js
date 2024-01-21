3ed082ad7d631b79f0d98b142ce448f1b1d67dec
Ivan Arkaev <75647984+ha308ing@users.noreply.github.com>
2024-01-21 23:23:41 +0400

    ground enemy

--- game/enemies.js
+++ game/enemies.js
@@ -80,8 +80,16 @@ export class FlyingEnemy extends Enemy {
 }
 
 export class GroundEnemy extends Enemy {
-  constructor() {
+  constructor(game) {
     super();
+    this.game = game;
+    this.width = 60;
+    this.height = 87;
+    this.x = this.game.width;
+    this.y = this.game.height - this.height - this.game.groundMargin;
+    this.frameLimit = 1;
+    this.speedX = 0;
+    this.speedY = 0;
     this.image = document.getElementById("enemy_plant");
   }
 }
